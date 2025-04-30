9481af0ea1cffc98dd611b6fbf53b69137893a21
R3ndrex <yacholohchtoli@gmail.com>
2025-04-30 14:08:25 +0300

    Change page h1 to stations

--- src/app/station/page.jsx
+++ src/app/station/page.jsx
@@ -3,7 +3,7 @@ import StationList from "./StationList";
 export default function AddStationPage() {
     return (
         <main className="flex flex-col justify-center items-center m-5">
-            <h1 className="text-2xl">Добавить станцию</h1>
+            <h1 className="text-2xl">Stations</h1>
             <AddStationForm />
             <StationList />
         </main>
