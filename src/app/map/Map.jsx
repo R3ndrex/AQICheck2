d688f043893567e92a13f28267f4d9cc0d35aead
R3ndrex <yacholohchtoli@gmail.com>
2025-05-02 13:16:43 +0300

    Fetch data from online service and database

--- src/app/map/Map.jsx
+++ src/app/map/Map.jsx
@@ -20,22 +20,26 @@ export default function Map({ position }) {
             position[0] + 0.5
         },${position[1] + 0.5}`;
 
-        fetch(
-            `https://api.waqi.info/v2/map/bounds/?latlng=${bounds}&token=${process.env.NEXT_PUBLIC_TOKEN}`
-        )
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.status === "ok") {
-                    setStations(data.data);
-                } else {
-                    console.error("Error loading stations:", data.data);
-                }
+        Promise.all([
+            fetch(
+                `https://api.waqi.info/v2/map/bounds/?latlng=${bounds}&token=${process.env.NEXT_PUBLIC_TOKEN}`
+            ),
+            fetch("/api/stations"),
+        ])
+            .then(async ([waqiRes, localRes]) => {
+                const waqiData = await waqiRes.json();
+                const localData = await localRes.json();
+
+                const waqiStations =
+                    waqiData.status === "ok" ? waqiData.data : [];
+                const combinedStations = [...waqiStations, ...localData];
+
+                setStations(combinedStations);
             })
             .catch((error) => {
-                console.error("Error while request:", error);
+                console.error("Error while requesting:", error);
             });
     }, [position]);
-
     function MapEvents({ setStations }) {
         const map = useMapEvents({
             moveend: () => {
@@ -44,22 +48,27 @@ export default function Map({ position }) {
                 const ne = bounds.getNorthEast();
                 const bbox = `${sw.lat},${sw.lng},${ne.lat},${ne.lng}`;
 
-                fetch(
-                    `https://api.waqi.info/v2/map/bounds/?latlng=${bbox}&token=${process.env.NEXT_PUBLIC_TOKEN}`
-                )
-                    .then((res) => res.json())
-                    .then((data) => {
-                        if (data.status === "ok") {
-                            setStations(data.data);
-                        } else {
-                            console.error(
-                                "Error loading new stations:",
-                                data.data
-                            );
-                        }
+                Promise.all([
+                    fetch(
+                        `https://api.waqi.info/v2/map/bounds/?latlng=${bbox}&token=${process.env.NEXT_PUBLIC_TOKEN}`
+                    ),
+                    fetch("/api/stations"),
+                ])
+                    .then(async ([waqiRes, localRes]) => {
+                        const waqiData = await waqiRes.json();
+                        const localData = await localRes.json();
+
+                        const waqiStations =
+                            waqiData.status === "ok" ? waqiData.data : [];
+                        const combinedStations = [
+                            ...waqiStations,
+                            ...localData,
+                        ];
+
+                        setStations(combinedStations);
                     })
                     .catch((error) => {
-                        console.error("Error while moving map:", error);
+                        console.error("Ошибка при обновлении карты:", error);
                     });
             },
         });
@@ -98,86 +107,110 @@ export default function Map({ position }) {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <MapEvents setStations={setStations} />
-            {stations.map((station) => (
-                <Marker
-                    key={station.uid}
-                    position={[station.lat, station.lon]}
-                    icon={L.icon({
-                        iconUrl: `https://waqi.info/mapicon/${station.aqi}.30.png`,
-                        iconSize: [40, 40],
-                        iconAnchor: [20, 40],
-                    })}
-                    eventHandlers={{
-                        click: () => loadPopupData(station.uid),
-                    }}
-                >
-                    <Popup>
-                        {loadingPopupUid === station.uid && (
-                            <div>Loading...</div>
-                        )}
+            {stations.map((station) => {
+                const isLocal = !!station._id && !station.uid;
+                const markerKey = station.uid || station._id;
 
-                        {popupData[station.uid] ? (
-                            <div style={{ minWidth: "200px" }}>
-                                <b>{popupData[station.uid].city.name}</b>
-                                <br />
-                                <b>AQI:</b> {popupData[station.uid].aqi}
-                                <br />
-                                <b>Updated:</b>{" "}
-                                {new Date(
-                                    popupData[station.uid].time.v * 1000
-                                ).toLocaleTimeString()}
-                                <br />
-                                <br />
-                                {popupData[station.uid].city.location && (
-                                    <>
-                                        <b>Location:</b>{" "}
-                                        {popupData[station.uid].city.location}
-                                        <br />
-                                        <br />
-                                    </>
-                                )}
-                                <b>Pollutants:</b>
-                                <br />
-                                {["pm25", "pm10", "o3", "no2", "so2", "co"].map(
-                                    (pollutant) =>
-                                        popupData[station.uid].iaqi?.[
-                                            pollutant
-                                        ] && (
-                                            <div key={pollutant}>
-                                                {pollutant.toUpperCase()}:{" "}
-                                                {
-                                                    popupData[station.uid].iaqi[
-                                                        pollutant
-                                                    ].v
-                                                }
+                return (
+                    <Marker
+                        key={markerKey}
+                        position={[station.lat, station.lon]}
+                        icon={L.icon({
+                            iconUrl: `https://waqi.info/mapicon/${station.aqi}.30.png`,
+                            iconSize: [40, 40],
+                            iconAnchor: [20, 40],
+                        })}
+                        eventHandlers={{
+                            click: () => {
+                                if (!isLocal) loadPopupData(station.uid);
+                            },
+                        }}
+                    >
+                        <Popup>
+                            {isLocal ? (
+                                <div style={{ minWidth: "200px" }}>
+                                    <b>{station.name}</b>
+                                    <br />
+                                    <b>AQI:</b> {station.aqi}
+                                    <br />
+                                    <b>Latitude:</b> {station.lat}
+                                    <br />
+                                    <b>Longitude:</b> {station.lon}
+                                    <br />
+                                    <i>Local station</i>
+                                </div>
+                            ) : loadingPopupUid === station.uid ? (
+                                <div>Loading...</div>
+                            ) : popupData[station.uid] ? (
+                                <div style={{ minWidth: "200px" }}>
+                                    <b>{popupData[station.uid].city.name}</b>
+                                    <br />
+                                    <b>AQI:</b> {popupData[station.uid].aqi}
+                                    <br />
+                                    <b>Updated:</b>{" "}
+                                    {new Date(
+                                        popupData[station.uid].time.v * 1000
+                                    ).toLocaleTimeString()}
+                                    <br />
+                                    <br />
+                                    {popupData[station.uid].city.location && (
+                                        <>
+                                            <b>Location:</b>{" "}
+                                            {
+                                                popupData[station.uid].city
+                                                    .location
+                                            }
+                                            <br />
+                                            <br />
+                                        </>
+                                    )}
+                                    <b>Pollutants:</b>
+                                    <br />
+                                    {[
+                                        "pm25",
+                                        "pm10",
+                                        "o3",
+                                        "no2",
+                                        "so2",
+                                        "co",
+                                    ].map(
+                                        (pollutant) =>
+                                            popupData[station.uid].iaqi?.[
+                                                pollutant
+                                            ] && (
+                                                <div key={pollutant}>
+                                                    {pollutant.toUpperCase()}:{" "}
+                                                    {
+                                                        popupData[station.uid]
+                                                            .iaqi[pollutant].v
+                                                    }
+                                                </div>
+                                            )
+                                    )}
+                                    <br />
+                                    <b>Source:</b>
+                                    <br />
+                                    {popupData[station.uid].attributions?.map(
+                                        (attr, idx) => (
+                                            <div key={idx}>
+                                                <a
+                                                    href={attr.url}
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                >
+                                                    {attr.name}
+                                                </a>
                                             </div>
                                         )
-                                )}
-                                <br />
-                                <b>Source:</b>
-                                <br />
-                                {popupData[station.uid].attributions?.map(
-                                    (attr, idx) => (
-                                        <div key={idx}>
-                                            <a
-                                                href={attr.url}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                {attr.name}
-                                            </a>
-                                        </div>
-                                    )
-                                )}
-                            </div>
-                        ) : (
-                            !loadingPopupUid && (
+                                    )}
+                                </div>
+                            ) : (
                                 <div>Press on marker to load...</div>
-                            )
-                        )}
-                    </Popup>
-                </Marker>
-            ))}
+                            )}
+                        </Popup>
+                    </Marker>
+                );
+            })}
         </MapContainer>
     );
 }
