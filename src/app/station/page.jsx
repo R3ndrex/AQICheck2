7ff7ce8f69ab659dd625bd51d59e7d7869021975
R3ndrex <yacholohchtoli@gmail.com>
2025-05-13 12:11:40 +0300

    make items wrap

--- src/app/station/page.jsx
+++ src/app/station/page.jsx
@@ -2,8 +2,7 @@ import AddStationForm from "./AddStationForm";
 import StationList from "./StationList";
 export default function AddStationPage() {
     return (
-        <main className="flex flex-col justify-center items-center mt-9">
-            <h1 className="text-2xl">Stations</h1>
+        <main className="flex flex-wrap justify-center items-top mt-9">
             <AddStationForm />
             <StationList />
         </main>
