be8df388fad3aa0e3958c1ce5b5c06bcb9aff504
R3ndrex <yacholohchtoli@gmail.com>
2025-05-13 11:56:04 +0300

    Change text to be smaller to display better on smaller screens

--- src/app/map/AQILegend.jsx
+++ src/app/map/AQILegend.jsx
@@ -1,7 +1,7 @@
 const levels = [
     { color: "#009966", label: "Good (0–50)" },
     { color: "#ffde33", label: "Moderate (51–100)" },
-    { color: "#ff9933", label: "Unhealthy for Sensitive Groups (101–150)" },
+    { color: "#ff9933", label: "Unhealthy for Sensitive (101–150)" },
     { color: "#cc0033", label: "Unhealthy (151–200)" },
     { color: "#660099", label: "Very Unhealthy (201–300)" },
     { color: "#7e0023", label: "Hazardous (301+)" },
