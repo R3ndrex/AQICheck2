9114c19912b2bb6064655b1fe59a3c2f1a5d7f46
R3ndrex <yacholohchtoli@gmail.com>
2025-05-22 21:15:09 +0300

    add semibold font change color of aqi number depending on aqi level

--- src/app/DataSection.jsx
+++ src/app/DataSection.jsx
@@ -1,13 +1,23 @@
 "use client";
 import PollutionForecast from "./PollutionForecast.jsx";
-
+import { getAQILevel } from "./map/AQILegend.jsx";
 export default function DataSection({ data }) {
     const DailyArray = Object.entries(data.data.forecast.daily);
     return (
         <main className="flex gap-5 flex-col items-center m-5">
             <section>
                 <h1 className="text-2xl">{data.data.city.name}</h1>
-                <p>AQI: {data.data.aqi}</p>
+                <p>
+                    AQI:{" "}
+                    <span
+                        className="font-semibold"
+                        style={{
+                            color: getAQILevel(data.data.aqi).color,
+                        }}
+                    >
+                        {data.data.aqi}
+                    </span>
+                </p>
             </section>
             {DailyArray.map(([pollutionName, pollutionValue]) => (
                 <PollutionForecast
