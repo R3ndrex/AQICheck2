5e1748c03c76da2cb29cf3c64e5b0bf6d1315dc8
R3ndrex <yacholohchtoli@gmail.com>
2025-05-27 17:00:06 +0300

    add aqi history

--- src/models/station.js
+++ src/models/station.js
@@ -4,8 +4,12 @@ const StationSchema = new mongoose.Schema({
     name: { type: String, required: true },
     lat: { type: Number, required: true },
     lon: { type: Number, required: true },
-    aqi: { type: Number, required: true },
+    aqiHistory: [
+        {
+            aqi: { type: Number, required: true },
+            recordedAt: { type: Date, default: Date.now },
+        },
+    ],
 });
-
 export default mongoose.models.Station ||
     mongoose.model("Station", StationSchema);
