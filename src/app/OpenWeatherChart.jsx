6dec21414a8efd3f5c485d38c89d2d2b605de492
R3ndrex <yacholohchtoli@gmail.com>
2025-05-27 15:44:56 +0300

    Add openweather charts

---
+++ src/app/OpenWeatherChart.jsx
@@ -0,0 +1,51 @@
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    Tooltip,
+    ResponsiveContainer,
+    CartesianGrid,
+} from "recharts";
+
+import formatDate from "./utils/formatDate";
+
+export default function OpenWeatherChart({ data, pollutant, color }) {
+    const chartData = data.list.map((entry) => {
+        const date = new Date(entry.dt * 1000);
+        return {
+            time: date.toISOString(),
+            value: entry.components[pollutant],
+        };
+    });
+
+    return (
+        <div className="graph h-[350px]">
+            <h2 className="text-lg font-semibold mb-2 capitalize">
+                {pollutant}
+            </h2>
+            <ResponsiveContainer width="100%" height={300}>
+                <LineChart data={chartData}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis
+                        dataKey="time"
+                        tickFormatter={formatDate}
+                        interval="preserveStartEnd"
+                    />
+                    <YAxis />
+                    <Tooltip
+                        labelFormatter={(label) => {
+                            return new Date(label).toLocaleString();
+                        }}
+                    />
+                    <Line
+                        type="monotone"
+                        dataKey="value"
+                        stroke={color}
+                        dot={false}
+                    />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    );
+}
---
+++ src/app/OpenWeatherForecast.jsx
@@ -0,0 +1,34 @@
+import OpenWeatherChart from "./OpenWeatherChart";
+
+export default function OpenWeatherForecast({ data }) {
+    const pollutants = {
+        co: "#8884d8",
+        no: "#82ca9d",
+        no2: "#ffc658",
+        o3: "#ff7300",
+        so2: "#a4de6c",
+        pm2_5: "#d0ed57",
+        pm10: "#8dd1e1",
+        nh3: "#888888",
+    };
+
+    return (
+        <>
+            {Object.entries(pollutants).map(([pollutant, color]) => (
+                <section className="inline-flex gap-5 w-[90%] flex-wrap items-center justify-center border-1 p-5">
+                    <div className="flex flex-col w-full items-center">
+                        <h2 className="text-2xl capitalize">
+                            {pollutant} Forecast
+                        </h2>
+                        <OpenWeatherChart
+                            key={pollutant}
+                            data={data}
+                            pollutant={pollutant}
+                            color={color}
+                        />
+                    </div>
+                </section>
+            ))}
+        </>
+    );
+}
